a632f0038ba27524b95c9392d3090107d8ae1629
Raghav1705 <Raghav1705@users.noreply.github.com>
2021-04-14 14:09:39 -0700

    Starter Code

--- utils.py
+++ utils.py
@@ -12,13 +12,15 @@ def is_valid_solution(G, c, k):
     """
     size = len(G)
     H = G.copy()
-    for city in c:
-        assert H.has_node(city), "Invalid Solution: {} is not a valid node in graph G".format(city)
-    H.remove_nodes_from(c)
 
     for road in k:
         assert H.has_edge(road[0], road[1]), "Invalid Solution: {} is not a valid edge in graph G".format(road)
     H.remove_edges_from(k)
+    
+    for city in c:
+        assert H.has_node(city), "Invalid Solution: {} is not a valid node in graph G".format(city)
+    H.remove_nodes_from(c)
+    
     assert H.has_node(0), 'Invalid Solution: Source vertex is removed'
     assert H.has_node(size - 1), 'Invalid Solution: Target vertex is removed'
 
